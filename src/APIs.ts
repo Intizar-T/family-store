04e255cdd27c0d13889ddd83166f683b46f85e01
Intizar Tashov <intizar.tashov22@gmail.com>
2023-03-21 13:51:00 +0300

    changed API endpoint url to onrender

--- src/APIs.ts
+++ src/APIs.ts
@@ -1,3 +1,2 @@
-export const BASE_URL =
-  "https://076efc83084203ddcbbe69ccc62c8640.loophole.site";
+export const BASE_URL = "https://family-store.onrender.com";
 export const PRODUCTS_URL = `${BASE_URL}/store`;

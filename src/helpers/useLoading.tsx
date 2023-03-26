43519505701e50d843a9737db8396e561fa2bf7c
Intizar Tashov <intizar.tashov22@gmail.com>
2023-03-26 18:00:00 +0300

    custom useLoading hook

---
+++ src/helpers/useLoading.tsx
@@ -0,0 +1,29 @@
+import { Backdrop, CircularProgress } from "@mui/material";
+import { FC, useState } from "react";
+import { createPortal } from "react-dom";
+
+export default function useLoading(): [FC, (state: boolean) => void] {
+  const [loading, setLoading] = useState(false);
+  const el = document.getElementById("modal");
+  const toggle = (state: boolean) => {
+    setLoading(state);
+  };
+  return [
+    () =>
+      loading
+        ? createPortal(
+            <Backdrop
+              sx={{
+                color: "#fff",
+                zIndex: (theme) => theme.zIndex.drawer + 1000,
+              }}
+              open={true}
+            >
+              <CircularProgress color="inherit" />
+            </Backdrop>,
+            el as HTMLElement
+          )
+        : null,
+    toggle,
+  ];
+}

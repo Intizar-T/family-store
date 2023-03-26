f2d84e16e1a3e6472dffc54f9fd420f1c382f2f6
Intizar Tashov <intizar.tashov22@gmail.com>
2023-03-26 23:43:11 +0300

    testing service worker - fix name typo

--- src/App.tsx
+++ src/App.tsx
@@ -16,9 +16,9 @@ export type User = {
 const registerServiceWorker = async () => {
   if ("serviceWorker" in navigator) {
     try {
-      const registration = await navigator.serviceWorker.register("sw.js", {
-        scope: "./",
-      });
+      const registration = await navigator.serviceWorker.register(
+        "service-worker.js"
+      );
       if (registration.installing) {
         console.log("Service worker installing");
       } else if (registration.waiting) {

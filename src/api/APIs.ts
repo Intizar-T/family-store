ac7a0497dd081167ac66bf8617c0c3017d0a7fe3
Intizar Tashov <intizar.tashov22@gmail.com>
2023-03-31 02:33:29 +0300

    add lambda endpoint

--- src/api/APIs.ts
+++ src/api/APIs.ts
@@ -1,5 +1,5 @@
-export const BASE_URL = "https://family-store.onrender.com";
-// const BASE_URL = "http://localhost:8002";
-// const BASE_URL = "http://192.168.1.109:8002";
+// export const BASE_URL = "https://z7x9jyq1mj.execute-api.ap-northeast-2.amazonaws.com/dev";
+// export const BASE_URL = "http://localhost:8002";
+export const BASE_URL = "http://192.168.1.109:8002";
 export const PRODUCTS_URL = `${BASE_URL}/store`;
 export const USER_URL = `${BASE_URL}/user`;

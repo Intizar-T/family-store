d5fc3fc3f2c51b2ae843cd312641df7cf153c3ea
Intizar Tashov <intizar.tashov22@gmail.com>
2023-04-20 18:02:08 +0300

    move constructing message logic to backend

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,12 +1,12 @@
 self.addEventListener("push", (event) => {
   const data = event.data.json();
-  let message = "";
-  if (data?.product != null)
-    message = `${data.user} sayta ${data.product} koshdy`;
-  else
-    message = `${data.user} hazyr magazina girjak bolotran. Garak zadynyzlary sayta yazynlar`;
-  const promiseChain = self.registration.showNotification("Saytdan tazalyk:", {
-    body: message,
-  });
-  event.waitUntil(promiseChain);
+  if (data.message != null) {
+    const promiseChain = self.registration.showNotification(
+      "Saytdan tazalyk:",
+      {
+        body: data.message,
+      }
+    );
+    event.waitUntil(promiseChain);
+  }
 });

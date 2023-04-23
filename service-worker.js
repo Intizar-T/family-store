dffb3d01f288b30b01dc6384f5445b1767b7cb5e
Intizar Tashov <intizar.tashov22@gmail.com>
2023-04-23 10:18:20 +0300

    Updates

--- service-worker.js
+++ service-worker.js
@@ -1,5 +1,6 @@
 self.addEventListener("push", (event) => {
   const data = event.data.json();
+  console.log(data);
   if (data.message != null) {
     const promiseChain = self.registration.showNotification(
       "Saytdan tazalyk:",

4909f6f8846ebe1cd7b576420707df9c4e66a2e6
Intizar Tashov <intizar.tashov22@gmail.com>
2023-04-23 22:19:50 +0300

    Updates

--- service-worker.js
+++ service-worker.js
@@ -11,3 +11,24 @@ self.addEventListener("push", (event) => {
     event.waitUntil(promiseChain);
   }
 });
+
+self.addEventListener("notificationclick", (event) => {
+  const url = "https://intizar-t.github.io/family-store/";
+  event.notification.close();
+  event.waitUntil(
+    clients.matchAll({ type: "window" }).then((windowClients) => {
+      // Check if there is already a window/tab open with the target URL
+      for (let i = 0; i < windowClients.length; i++) {
+        let client = windowClients[i];
+        // If so, just focus it.
+        if (client.url === url && "focus" in client) {
+          return client.focus();
+        }
+      }
+      // If not, then open the target URL in a new window/tab.
+      if (clients.openWindow) {
+        return clients.openWindow(url);
+      }
+    })
+  );
+});
